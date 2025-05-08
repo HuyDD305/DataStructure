0e9df2145e3177d5f51dd297fec1c282a13e919e
HuyDD305 <bribribridu@gmail.com>
2025-05-08 21:43:21 +0700

    recursion

---
+++ recursion/calculate_sum.py
@@ -0,0 +1,11 @@
+def sum(arr):
+    if isinstance(arr, list):
+        if arr:
+            return arr.pop() + sum(arr)
+        else:
+            return 0
+
+
+
+if __name__ == "__main__":
+    print(sum([2, 4, 6]))
